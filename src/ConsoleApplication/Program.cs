a40714b25190af14c210cbb16838716822c40930
SANTIAGO ALONSO <SANTIAGO.RALONSO@correo.ucu.edu.uy>
2021-08-30 23:38:28 -0300

    Hecho punto 8, respondido el punto 9 como comentario

--- src/ConsoleApplication/Program.cs
+++ src/ConsoleApplication/Program.cs
@@ -24,10 +24,27 @@ namespace ConsoleApplication
                 Train test = new Train($"Tren n°: {i}");
             }
             Console.WriteLine($"{Train.CantidadTrenes}");
-        }
+            
+        /// <summary>
         /// Se generan las 100 instancias de trenes exitosamente, pero al intentar inicializarse 10 millones, esto supera la capacidad de la memoria heap para almacenar objetos
-        /// por lo que se destruyen  para hacer lugar para los nuevos, la cantidad maxima que sobrevivieron en mi caso fueron 9972287 */
+        /// por lo que se destruyen  para hacer lugar para los nuevos, la cantidad maxima que sobrevivieron en mi caso fueron 9972287 
+        /// <summary>
+        */
 
+        Train t1 = new Train ("Last Train to London");
+        Train t2 = new Train ("Last Train to London");
+        Train t3 = new Train ("Runaway Train");
+
+        Console.WriteLine($"Caso 1: t1==t2: {t1==t2}");
+        Console.WriteLine($"Caso 1: t3==t2: {t3==t2}");
         
+        /// <summary>
+        /// Ambas devuelven false: El primer caso es debido a que se crean dos objetos distintos en direcciones diferentes, por lo que a pesar de que su estado es tecnicamente el mismo,
+        /// son objetos diferentes con lugares diferentes en la memoria.
+        /// El segundo caso ocurre lo mismo, aunque tambien añadido que el estado de ambas instancias es diferente, pero la diferencia que provoca esta respuesta es el mismo del caso
+        /// anterior.
+         /// <summary>
+        }
+
     }
 }
