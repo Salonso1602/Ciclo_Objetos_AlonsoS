04ca44e552c5c1aaf2bea0705cf08b82b5fc8cb9
SANTIAGO ALONSO <SANTIAGO.RALONSO@correo.ucu.edu.uy>
2021-08-30 23:46:36 -0300

    emprolijado gral y teminaci+on

--- src/ClassLibrary/Train.cs
+++ src/ClassLibrary/Train.cs
@@ -8,47 +8,22 @@ using System;
 
 namespace ClassLibrary
 {
-    /// <summary>
-    /// Esta clase representa un tren muy básico.
-    /// </summary>
     public class Train
-    {
-        /// <summary>
-        /// Obtiene un valor que indica si las maquinas del tren han sido encendidas o no.
-        /// </summary>
-        /// <value><c>true</c> si las máquinas fueron encendidas, <c>false</c> en caso contrario.</value>
-        public bool IsEngineStarted { get; private set; }
-
-        /// <summary>
-        /// Enciende las máquinas del tren.
-        /// </summary>
-        /// <returns>
-        /// <c>true</c> si las máquinas pueden ser encendidas, <c>false</c> en caso contrario.
-        /// </returns>
-        
+    {  
         private static int Count = 0;
 
-        private string Name;
+        public string Name {get; set;}
         
         public Train(string name)
         {
             this.Name = name;
             CantidadTrenes++;
-
         }
 
         ~Train()
         {
             Console.WriteLine($"RIP {this.Name}");
             CantidadTrenes--;
-        }
-
-        public string Nombre
-        {
-            get
-            {
-                return this.Name;
-            }
         }
 
         public static int CantidadTrenes
@@ -62,6 +37,18 @@ namespace ClassLibrary
                 Count = value;
             }
         }
+
+        /// <summary>
+        /// Obtiene un valor que indica si las maquinas del tren han sido encendidas o no.
+        /// </summary>
+        /// <value><c>true</c> si las máquinas fueron encendidas, <c>false</c> en caso contrario.</value>
+        public bool IsEngineStarted {get; private set;}
+        /// <summary>
+        /// Enciende las máquinas del tren.
+        /// </summary>
+        /// <returns>
+        /// <c>true</c> si las máquinas pueden ser encendidas, <c>false</c> en caso contrario.
+        /// </returns>
 
         public bool StartEngines()
         {
--- src/ConsoleApplication/Program.cs
+++ src/ConsoleApplication/Program.cs
@@ -9,27 +9,27 @@ using ClassLibrary;
 
 namespace ConsoleApplication
 {
-    /// <summary>
-    /// Programa de consola de demostración.
-    /// </summary>
     public static class Program
     {
-        /// <summary>
-        /// Punto de entrada al programa principal.
-        /// </summary>
         public static void Main()
         {
-           /* for (int i = 0; i<10000000; i++)
+
+
+        /// <summary> 
+        ///    for (int i = 0; i<10000000; i++)
             {
-                Train test = new Train($"Tren n°: {i}");
+        ///        Train test = new Train($"Tren n°: {i}");
             }
-            Console.WriteLine($"{Train.CantidadTrenes}");
-            
-        /// <summary>
+        ///    Console.WriteLine($"{Train.CantidadTrenes}");
+
+        
         /// Se generan las 100 instancias de trenes exitosamente, pero al intentar inicializarse 10 millones, esto supera la capacidad de la memoria heap para almacenar objetos
         /// por lo que se destruyen  para hacer lugar para los nuevos, la cantidad maxima que sobrevivieron en mi caso fueron 9972287 
-        /// <summary>
-        */
+        /// </summary>
+        
+
+
+
 
         Train t1 = new Train ("Last Train to London");
         Train t2 = new Train ("Last Train to London");
